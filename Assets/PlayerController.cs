0f37ad9b5a3f78b534de3ac0b98a3f7fbc98fd74
Commkeen <commkeen4000@gmail.com>
2019-08-11 17:57:06 -0700

    Remove extra powerup spawn

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -132,7 +132,6 @@ public class PlayerController : MonoBehaviour
     void ExpireSuperpower()
     {
         _animator.runtimeAnimatorController = mouseAnimator;
-        PowerupSpawnController.Instance.SpawnPowerup();
         MusicManager.Instance.FadeOutPowerupMusic();
         PowerupSpawnController.Instance.ReplenishPowerups();
     }
