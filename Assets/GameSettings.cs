def1a3d6421e477fd9a965b58611da92bf16d8ae
candlesan <wyattcheng@gmail.com>
2019-08-11 17:45:16 -0700

    Names and fonts

--- Assets/GameSettings.cs
+++ Assets/GameSettings.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class GameSettings
 {
+    static public readonly string[] PlayerNames =
+    {
+        "Cecil",
+        "Cashew",
+        "Cooper",
+        "Izzy"
+
+    };
     static public bool[] PlayerIsActive = new bool[4];
 }
